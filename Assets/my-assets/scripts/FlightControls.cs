4658352985b28c458db45b2b799a6deea557735d
jmanderson0119 <janderson011903@gmail.com>
2023-06-30 21:29:53 -0400

    Update FlightControls.cs
    added flight sim controls alongside fps controls

--- Assets/my-assets/scripts/FlightControls.cs
+++ Assets/my-assets/scripts/FlightControls.cs
@@ -2,91 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// FlightControls allows the player the pitch, yaw, and roll in the level
+// FlightControls allows the player the pitch, yaw, roll, and throttle in the level
 public class FlightControls : MonoBehaviour
 {
+    [Header("Input Mode")]
+    [SerializeField] private InputMode inputMode;
 
-    [SerializeField] private Rigidbody playerBody; // player's rb
+    [Header("Input Deadzones")]
+    [SerializeField] private int mouseLook;
 
-    [SerializeField] private float pitchSpeed; // vertical sensitivity
-    [SerializeField] private float yawSpeed; // horizontal sensitivity
-    [SerializeField] private float rollSpeed; // roll sensitivity
+    [Header("Input Speeds")] // player movement sensitivities
+    [SerializeField] private float throttleSpeed;
+    [SerializeField] private float pitchSpeed;
+    [SerializeField] private float yawSpeed;
+    [SerializeField] private float rollSpeed;
 
-    [SerializeField] private float acceleration; // Acceleration rate
-    [SerializeField] private float deceleration; // Deceleration rate
-    [SerializeField] private float maxSpeed; // Maximum speed
+    [Header("Input Accelerations")] // player movement accelerations (see Lerps)
+    [SerializeField] private float throttleAcceleration;
+    [SerializeField] private float rollAcceleration;
 
-    private float currentSpeed = 0f; // Current speed
-    private float pitchIn; // player pitch input
-    private float yawIn; // player yaw input
-    private float rollIn; // player roll input
-    private float throttleIn; // player throttle input
-    private Quaternion rotation; // player rotation update quaternion
+    [Header("Input Data Readout")] // player movement readouts
+    [SerializeField] private float throttleIn;
+    [SerializeField] private float rollIn;
+    [SerializeField] private Vector2 mouseIn;
 
+    private Vector2 lookIn; // mouse input relative to the screen center
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked; // hide the cursor and center it upon entering the level
+        // cursor becomes invisible + confined to the game view
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
     }
-
 
     void Update()
     {
-        /*
-     	* This is the pitch/yaw/roll mechanic. It reads, by frame, the mouse movement, which is then used
-     	* to calculate a quaternion. This quaternion describes a rotation about the ship's axes based on the mouse sensitivity,
-     	* the amount of mouse movement, and time between frames. This is then multiplied by the player's current orientation
-     	* to concatenate them and yield the updated orientation:
-     	*/
+        // calculate throttle as a lerp between current throttle magnitude and the target throttle input, over the throttle acceleration by frame
+        throttleIn = Mathf.Lerp(throttleIn, Input.GetAxisRaw("Throttle") * throttleSpeed, throttleAcceleration * Time.deltaTime);
 
-        pitchIn = Input.GetAxis("Mouse Y"); // step 1.1: get vertical mouse input
-        yawIn = Input.GetAxis("Mouse X"); // step 1.2: get horizontal mouse input
-
-        // step 1.3: get the 'A' and 'D' key input
-        if (Input.GetKey(KeyCode.A))
+        // calculate pitch and yaw accoring to player preferences
+        if (inputMode == InputMode.FlightSim)
         {
-            rollIn = 1f; // rotate CCW
+            // how far in the x and y the mouse is from the center of the screen
+            lookIn.x = (float) (Input.mousePosition.x - (0.5 * Screen.width));
+            lookIn.y = (float) (Input.mousePosition.y - (0.5 * Screen.height));
+
+            // set the direction the mouse is pointing in relative to the center; snap to center if it's close enough
+            mouseIn.x = Mathf.Abs(lookIn.x) <= mouseLook ? 0f : (float) ((lookIn.x) / (0.5 * Screen.height));
+            mouseIn.y = Mathf.Abs(lookIn.y) <= mouseLook ? 0f : (float) ((lookIn.y) / (0.5 * Screen.height));
+
+            // calculate pitch and yaw as a normalized vector preserving the proportions between their magnitudes
+            Vector3.Normalize(mouseIn);
+        }
+        else
+        {
+            // calculate pitch and yaw from mouse input tracking
+            mouseIn.x = Input.GetAxis("Mouse X");
+            mouseIn.y = Input.GetAxis("Mouse Y");
         }
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            rollIn = -1f; // rotate CW
-        }
+        // calculate roll as a lerp between the current roll magnitude and the target roll input, over the roll acceleration by frame
+        rollIn = Mathf.Lerp(rollIn, Input.GetAxisRaw("Roll") * rollSpeed, rollAcceleration * Time.deltaTime);
 
-        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
-        {
-            rollIn = 0f;
-        }
-
-        rotation = Quaternion.Euler(-pitchIn * pitchSpeed * Time.deltaTime, yawIn * yawSpeed * Time.deltaTime, rollIn * rollSpeed * Time.deltaTime); // step 2: calculate the rotation update quaternion
-
-        transform.rotation *= rotation; // step 3: rotate the ship
-
-        /*
-     	* This is the throttle mechanic. It reads the keybind activity of 'W' and 'S' and applies a constant acceleration with consideration
-     	* to max speed of the ship, the acceleration and deceleration set on the ship, and the time between frames.
-     	*/
-
-        throttleIn = Input.GetAxis("Vertical"); // Get throttle input
-
-        if (throttleIn > 0f)
-        {
-            // Accelerate the ship
-            currentSpeed += acceleration * Time.deltaTime;
-
-            // Limit the speed to the maximum speed
-            currentSpeed = Mathf.Clamp(currentSpeed, 0f, maxSpeed);
-        }
-        else if (throttleIn < 0f)
-        {
-            // Decelerate the ship
-            currentSpeed -= deceleration * Time.deltaTime;
-
-            // Limit the speed to zero
-            currentSpeed = Mathf.Clamp(currentSpeed, 0f, maxSpeed);
-        }
-
-        // Move the ship forward based on the current speed
-        transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
+        // rotate and translate the ship at the corresponding speed by frame
+        transform.Rotate(-mouseIn.y * pitchSpeed * Time.deltaTime, mouseIn.x * yawSpeed * Time.deltaTime, rollIn * Time.deltaTime, Space.Self);
+        transform.position += transform.forward * throttleIn * Time.deltaTime;
+    }
+    
+    // selection between different types of mouse look calculations
+    private enum InputMode
+    {
+        FPS,
+        FlightSim
     }
 }
