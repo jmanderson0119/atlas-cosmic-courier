f62297aac4514b45ac16c6d3d3a9f31bebfe6c18
jmanderson0119 <janderson011903@gmail.com>
2023-07-13 13:50:44 -0400

    Update DebrisSpawner
    fixed up some incorrect logic, looking pretty good now but will touch up once im off shift

--- Assets/my-assets/scripts/DebrisLauncher.cs
+++ Assets/my-assets/scripts/DebrisLauncher.cs
@@ -12,48 +12,58 @@ public class DebrisLauncher : MonoBehaviour
     [Header("Objects to Lauch")]
     [SerializeField] private List<GameObject> debris; // launchable object references
 
-    // the need to know's: the object to be launched, its rigidbody, its starting location, how its spinning, and how its moving
+    // logistics of launch: object, its accuracy, its RB, position relative to ship, spawn location in world space
     private GameObject spawnedObject;
+    private int objectAccuracy;
     private Rigidbody rigidBody;
-    private Vector2 spawnRelativeToShipXAndY;
     private Vector3 spawnRelativeToShip;
     private Vector3 spawnLocation;
-    private Vector3 spawnTorque;
-    private Vector3 spawnForce;
 
-    private int objectSpeed;
-    private float timeToReceive;
+    // connection logistics: speed of debris, time until reception, position at reception
     private Vector3 positionToReceive;
 
     // launch() to be called every so often once the player is situated in the level
     void Start()
     {
-        InvokeRepeating("launch", 10f, Random.Range(3f, 8f));
+        InvokeRepeating("launch", 2f, Random.Range(1f, 6f));
     }
 
+    // guesses the position of the targeted object and launches a piece of debris in that direction
     void launch()
     {
         Debug.Log("Fire");
 
         // position: select a random point in a circle placed in front of the player
-        spawnRelativeToShipXAndY = Random.insideUnitCircle * 500f;
-        spawnRelativeToShip = new Vector3(ship.transform.position.x + spawnRelativeToShipXAndY.x, ship.transform.position.y + spawnRelativeToShipXAndY.y, ship.transform.position.z + 2000f);
+        spawnRelativeToShip = Random.insideUnitCircle * 500f;
+        spawnLocation = ship.transform.position + ship.transform.right * spawnRelativeToShip.x + ship.transform.up * spawnRelativeToShip.y + ship.transform.forward * 2000f;
 
-        spawnLocation = ship.transform.right * spawnRelativeToShip.x + ship.transform.up * spawnRelativeToShip.y + ship.transform.forward * spawnRelativeToShip.z;
-
-        Debug.Log("(" + spawnLocation.x + ", " + spawnLocation.y + ", " + spawnLocation.z + ")");
-        
         // spawn & initialization
         spawnedObject = Instantiate(debris[Random.Range(0, debris.Count)], spawnLocation, Quaternion.identity);
         rigidBody = spawnedObject.GetComponent<Rigidbody>();
 
-        // apply torque & force
-        objectSpeed = Random.Range(250, 500);
-        timeToReceive = Vector3.Distance(ship.transform.position, spawnedObject.transform.position) / (objectSpeed);
-        positionToReceive = ship.transform.position + ship.transform.forward * flightData.getThrottleIn() * timeToReceive;
+        objectAccuracy = Random.Range(1, 21); // assign an accuracy rating
 
+
+        // 80% of the time - accurate, 10% - fairly accurate, 5% - somewhat accurate, 5% - somewhat inaccurate
+        if (objectAccuracy <= 16)
+        {
+            positionToReceive = ship.transform.position + ship.transform.forward * 2082.8125f + Random.insideUnitSphere * 67.1875f;
+        }
+        else if (objectAccuracy <= 18)
+        {
+            positionToReceive = ship.transform.position + ship.transform.forward * 2015.625f + Random.insideUnitSphere * 134.375f;
+        }
+        else if (objectAccuracy == 19)
+        {
+            positionToReceive = ship.transform.position + ship.transform.forward * 1881.25f + Random.insideUnitSphere * 268.75f;
+        }
+        else
+        {
+            positionToReceive = ship.transform.position + ship.transform.forward * 1612.5f + Random.insideUnitSphere * 537.5f;
+        }
+
+        // calculate appropriate force & torque
         rigidBody.AddTorque(Random.onUnitSphere, ForceMode.VelocityChange);
-        rigidBody.AddForce((positionToReceive - spawnedObject.transform.position).normalized * objectSpeed, ForceMode.VelocityChange);
-        
+        rigidBody.AddForce((positionToReceive - spawnedObject.transform.position).normalized * 350, ForceMode.VelocityChange);
     }
 }
